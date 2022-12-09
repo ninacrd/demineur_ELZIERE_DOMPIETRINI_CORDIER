13a458f39c5d61c03e33d4bdf67f5690dd82d586
Nina Cordier <nina.cordier@epfedu.fr>
2022-12-09 11:56:51 +0100

    création de la classe grille avec les bombes, drapeaux, kit

--- src/demineur_elziere_dompietrini_cordier/grille.java
+++ src/demineur_elziere_dompietrini_cordier/grille.java
@@ -9,5 +9,54 @@ package demineur_elziere_dompietrini_cordier;
  * @author odomp
  */
 public class grille {
+    /*creation d'une nouvelle classe*/
+
+    private boolean presence_bombe;
+    private boolean presence_Kit;
+    private boolean drapeau;
+    public int nombre_bombes;
+    
+    public grille(){ /*initialisation des attributs*/
+        presence_bombe = false; 
+        presence_Kit = false;
+        drapeau = false;
+        nombre_bombes = 0;
+    }
+    
+    public boolean presence_bombre(){ /*méthode qui permettra de vérifier la présence ou non d'une bombe dans la case et retourne un booléen*/
+        if (presence_bombe == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean presence_Kit(){ /*on vérifie la présence d'un Kit*/
+        if (presence_Kit == true){
+            return true;
+        } else {
+            return false; 
+        }
+    }
+    
+    public void placer_bombe(){ /*permet de placer une bombe*/
+        presence_bombe = true; /*renvoie vrai car on a une bombe*/
+    }
+    
+    public void placer_Kit(){
+        presence_Kit = true;
+    }
+    
+    public void placer_drapeau(){ /*permet de placer un drapeau à l'endroit où le joueur pense qu'il y a une bombe*/
+        drapeau = true;
+    }
+    
+    public void supprimer_drapeau(){
+        drapeau = false;
+    }
+    
+    public int nombre_bombes(){
+        return nombre_bombes;
+    }
     
 }
